d9d05e91bf81d9e9aedba4c47a8e95216fd108aa
Jay Tailor <tailorj64@gmail.com>
2019-05-22 16:21:49 +0530

    Created reducer and added to store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers/index';
+
 const App = () => {
   return(
-      <View/>
+      <Provider store={createStore(reducer)}>
+        <View/>
+      </Provider>
   )
 };
 
